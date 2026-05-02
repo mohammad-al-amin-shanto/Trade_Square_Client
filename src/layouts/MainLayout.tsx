760a8b9e2096a822c4609c80404a85ec12907754
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-05-02 23:02:47 +0600

    added Main Layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <main className="min-h-screen p-4">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
