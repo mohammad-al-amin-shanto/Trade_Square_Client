d2e4a8ca57dc06cf2c85ab207fe1f4d9d0ac3d23
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-05-02 23:05:29 +0600

    added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <section className="text-center py-20">
+        <h1 className="text-4xl font-bold">Simple trading. Smarter choices.</h1>
+        <button className="mt-4 bg-accent px-6 py-2 rounded text-white">
+          Browse Items
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
