63fe9074840a9eac5390024766af5b78d786e8c7
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-05-03 02:04:06 +0600

    fixed authProvider issue on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
-
-
+import AuthProvider from "./context/AuthProvider"
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AuthProvider>
+  </StrictMode>
 )
