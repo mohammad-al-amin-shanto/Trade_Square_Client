2cffc68673027acdfb3faa8eb7e9b4a10bfe2190
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-05-02 23:01:51 +0600

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import router from "./routes/router";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Items from "../pages/Items";
+import ItemDetails from "../pages/ItemDetails";
+import About from "../pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/items", element: <Items /> },
+      { path: "/items/:id", element: <ItemDetails /> },
+      { path: "/about", element: <About /> },
+    ],
+  },
+]);
+
+export default router;
